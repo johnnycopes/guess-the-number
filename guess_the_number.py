bf6410a3bc497069614d2efafecb934cb0a78310
John Coppola <jdcoppola@gmail.com>
2016-09-22 14:52:05 -0400

    adds more functionality to the game, including guess counter

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,50 +1,77 @@
-# Import 'random' module and generate random number
-
-from random import randint
-target_number = randint(1, 10)
-
-
-# Set variable with text indicating when user needs to write
-
+# Import 'random' module
+import random
+# Set playing to True while playing the game
+playing = True
+# Set variable with text to prompt user input
 prompt = "> "
 
-
-# Prompt the user to enter a guess and store it
-
-print """
-Welcome to the guessing game!
-I'm thinking of a number between 1 and 10.
-Can you guess what it is?
-"""
-
-user_guess = int(raw_input(prompt))
-
-
-# Declare function that prints appropriate instructions and
-# stores the user's next guess
-
-def instructions(message):
-    global user_guess
-    print message
-    user_guess = int(raw_input(prompt))
-
-
-# Set variables storing different instructions for logic
-
-too_low = "Too low! Try again."
-too_high = "Too high! Try again."
-out_of_range = "Out of range! Guess again, \
-this time between 1 and 10."
-
+# Define a function asking if the user wants
+# to play again
+def ask_to_play_again():
+    valid_answer = False
+    while valid_answer == False:
+        play_again = raw_input("Enter 'Y' or 'N' : ")
+        play_again = play_again.upper()
+        if play_again == "Y":
+            valid_answer = True
+            print "Great! Resetting..."
+        elif play_again == "N":
+            valid_answer = True
+            print "OK. Bye!"
+            global playing
+            playing = False
 
 # Game logic
 
-while user_guess != target_number:
-    if user_guess < target_number and user_guess > 0:
-        instructions(too_low)
-    elif user_guess > target_number and user_guess < 11:
-        instructions(too_high)
-    else:
-        instructions(out_of_range)
+while playing == True:
+    # Generate random number and store it
+    target_number = random.randint(1, 10)
+    # Store number of remaining available user guesses
+    num_guesses = 5
+    # Prompt the user to enter a guess and store it in a variable
+    print """
+Welcome to the guessing game!
+I'm thinking of a number between 1 and 10.
+You have 5 guesses. Can you guess what it is?
+    """
+    
+    while num_guesses > 0:
 
-print "Congratulations, you win!"
+        # Print number of guesses remaining and subtract 1
+        if num_guesses == 1:
+            print "You have %d guess remaining." % num_guesses
+        elif num_guesses < 5:
+            print "You have %d guesses remaining." % num_guesses
+        num_guesses -= 1
+        # Ask the user for a guess and store it
+        user_guess = int(raw_input(prompt))
+
+        # Give user feedback on their guess. If they have no
+        # guesses remaining, don't tell them to try again
+        if user_guess == target_number:
+            num_guesses = -1
+            print "Congratulations, you win!"
+        elif user_guess < target_number and user_guess > 0:
+            if num_guesses >= 1:
+                print "Too low! Try again."
+            else:
+                print "Too low!"
+        elif user_guess > target_number and user_guess < 11:
+            if num_guesses >= 1:
+                print "Too high! Try again."
+            else:
+                print "Too high!"
+        else:
+            if num_guesses >= 1:
+                print "Out of range! Guess again, \
+this time between 1 and 10."
+            else:
+                print "Out of range!"
+
+    # If user ran out of guesses, tell them
+    if num_guesses == 0:
+        print "You're out of guesses! Better luck next time."
+
+    # Ask the user if they want to play again
+    print "Want to play again?"
+    ask_to_play_again()
